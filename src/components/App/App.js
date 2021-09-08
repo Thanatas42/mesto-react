b9952da8e97a56f0db62ba576bf30f577b459708
Dmitry Dudin <dmitrodonskoy@yandex.ru>
2021-09-08 22:49:22 +0300

    Настройка попапов, виделение в компоненты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from '../header/Header';
-import Main from '../main/Main';
-import Footer from '../footer/Footer';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 import './App.css';
 
 function App() {
