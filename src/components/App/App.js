3d3399a13a1dfbb45032ff3bce1ba8e6ecf0d84a
Dmitry Dudin <dmitrodonskoy@yandex.ru>
2021-09-11 19:14:27 +0300

    Сборка проекта

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import './App.css';
 
 function App() {
+  const [isEditProfilePopupOpen, setEditOpen] = React.useState(false);
+  const [isAddPlacePopupOpen, setAddOpen] = React.useState(false);
+  const [isEditAvatarPopupOpen, setAvatarOpen] = React.useState(false);
+
+  function handleEditProfileClick() {
+    setEditOpen(true);
+  }
+  function handleAddPlaceClick() {
+    setAddOpen(true);
+  }
+  function handleEditAvatarClick() {
+    setAvatarOpen(true);
+  }
+  function closeAllPopups() {
+    setAvatarOpen(false);
+    setAddOpen(false);
+    setEditOpen(false);
+  }
+
   return (
     <div className="App body">
       <Header />
-      <Main />
+      <Main onEditProfile={handleEditProfileClick} isAddPlacePopupOpen={handleAddPlaceClick} isEditAvatarPopupOpen={handleEditAvatarClick} closeAllPopups={closeAllPopups}
+        editOpen={isEditProfilePopupOpen} addOpen={isAddPlacePopupOpen} avatarOpen={isEditAvatarPopupOpen} />
       <Footer />
     </div>
   );
